9897180ebc0436c7a710099e6c092939142e4a33
Medet Inkarbekov <54040250+ongarbai@users.noreply.github.com>
2024-11-27 11:10:15 +0000

    Initial website upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// If you need interactivity, you can add JavaScript here.
+// For example, you can implement the testimonial carousel functionality.
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Code for testimonial carousel
+  });
+  
